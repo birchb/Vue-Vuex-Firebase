ca3c2bbaae826f922d485be08cd930f108ad283c
birchb <birchb@users.noreply.github.com>
2018-02-11 14:59:26 -0500

    Simple proof of connection between codesandbox.io and github

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,20 @@
     <hello></hello>
     <message></message>
     <p>
-      Welcome to your Vue.js app!
+      Welcome!
     </p>
     <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
+      This is a simple app that shows integration with Firebase.
     </p>
     <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
+    I'm also trying to prove that I can make changes to this repository from codesandbox.io.
     </p>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
-import Message from './components/message'
+import Message from './components/Message'
 import store from './vuex/store'
 
 export default {
